981b5cfc152086e5e122e1d04f322784093cc5e9
Hugo <hugo.nobreg@gmail.com>
2016-06-27 20:53:00 -0300

    Primeiro commit

---
+++ src/test/java/br/com/drogariav3/util/HibernateUtilTest.java
@@ -0,0 +1,16 @@
+package br.com.drogariav3.util;
+
+import org.hibernate.Session;
+import org.junit.Test;
+
+public class HibernateUtilTest {
+	
+	@Test
+	public void conectar(){
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		sessao.close();
+		HibernateUtil.getFabricaDeSessoes().close();
+		
+	}
+
+}

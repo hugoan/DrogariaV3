21f311f407866f0e1d80a96040afa8b3fe37982d
Hugo <hugo.nobreg@gmail.com>
2016-06-27 21:27:53 -0300

    Criação da classe Pessoa na pacote domain Mapeamento da classe Pessoa no Hibernate.CFG

--- src/main/java/br/com/drogariav3/domain/Pessoa.java
+++ src/main/java/br/com/drogariav3/domain/Pessoa.java
@@ -2,6 +2,8 @@ package br.com.drogariav3.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @SuppressWarnings("serial")
 @Entity
@@ -39,6 +41,10 @@ public class Pessoa extends GenericDomain {
 
 	@Column(length = 100, nullable = false)
 	private String email;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Cidade cidade;
 
 	public String getNome() {
 		return nome;
@@ -126,6 +132,14 @@ public class Pessoa extends GenericDomain {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public Cidade getCidade() {
+		return cidade;
+	}
+	
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
 	}
 
 }

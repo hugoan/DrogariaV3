ec694a589d1d903847d06d7df75ee9e7a44f3c87
Particular <Particular@Particular-PC>
2016-06-29 22:27:14 -0300

    Implementação do método buscar por código usando Criteria.

--- src/main/java/br/com/drogariav3/dao/GenericDAO.java
+++ src/main/java/br/com/drogariav3/dao/GenericDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 import br.com.drogariav3.util.HibernateUtil;
 
@@ -58,4 +59,20 @@ public class GenericDAO<Entidade> {
 			sessao.close();
 		}
 	}
+
+	@SuppressWarnings("unchecked")
+	public Entidade buscar(Long codigo) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		try {
+			Criteria consulta = sessao.createCriteria(classe);
+			consulta.add(Restrictions.idEq(codigo));
+			Entidade resultado = (Entidade) consulta.uniqueResult();
+			return resultado;
+		} catch (RuntimeException erro) {
+			throw erro;
+		} finally {
+			sessao.close();
+		}
+	}
+
 }

80f5491e88ee17a2448aca1d7a623e9bc47d61f5
Hugo <hugo.nobreg@gmail.com>
2016-06-29 21:35:46 -0300

    implementação da api reflect para apoiar os algoritmos de pesquisa e listar

--- src/main/java/br/com/drogariav3/dao/GenericDAO.java
+++ src/main/java/br/com/drogariav3/dao/GenericDAO.java
@@ -1,11 +1,24 @@
 package br.com.drogariav3.dao;
 
+import java.lang.reflect.ParameterizedType;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import br.com.drogariav3.util.HibernateUtil;
 
+
 public class GenericDAO<Entidade> {
+	
+	//Comando para capturar a classe. Este comando é necessário para apoiar os algoritimos de pesquisa e listar
+	
+	private Class<Entidade> classe;
+	@SuppressWarnings("unchecked")
+	public GenericDAO(){
+		this.classe = (Class<Entidade>) ((ParameterizedType) getClass()
+				.getGenericSuperclass()).getActualTypeArguments()[0];
+	}
+	//Fim do comando
 
 	public void salvar(Entidade entidade) {
 		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();

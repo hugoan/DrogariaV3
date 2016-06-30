3b5d9117e0ffeb44a907ec8fb74c5a5329ded03e
Hugo <hugo.nobreg@gmail.com>
2016-06-29 21:59:18 -0300

    Implementação do método listar

--- src/main/java/br/com/drogariav3/dao/GenericDAO.java
+++ src/main/java/br/com/drogariav3/dao/GenericDAO.java
@@ -1,24 +1,28 @@
 package br.com.drogariav3.dao;
 
 import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import br.com.drogariav3.util.HibernateUtil;
 
-
 public class GenericDAO<Entidade> {
-	
-	//Comando para capturar a classe. Este comando é necessário para apoiar os algoritimos de pesquisa e listar
-	
+
+	// Comando para capturar a classe. Este comando é necessário para apoiar os
+	// algoritimos de pesquisa e listar
+
 	private Class<Entidade> classe;
+
 	@SuppressWarnings("unchecked")
-	public GenericDAO(){
+	public GenericDAO() {
 		this.classe = (Class<Entidade>) ((ParameterizedType) getClass()
 				.getGenericSuperclass()).getActualTypeArguments()[0];
 	}
-	//Fim do comando
+
+	// Fim do comando
 
 	public void salvar(Entidade entidade) {
 		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
@@ -34,6 +38,21 @@ public class GenericDAO<Entidade> {
 				transacao.rollback();
 			}
 
+			throw erro;
+		} finally {
+			sessao.close();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Entidade> listar() {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+
+		try {
+			Criteria consulta = sessao.createCriteria(classe);
+			List<Entidade> resultado = consulta.list();
+			return resultado;
+		} catch (RuntimeException erro) {
 			throw erro;
 		} finally {
 			sessao.close();

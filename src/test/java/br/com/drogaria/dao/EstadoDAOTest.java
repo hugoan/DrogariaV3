9f5e1999b17efe7a1fe427dddcb992118a0ca7a6
Hugo <hugo.nobreg@gmail.com>
2016-07-03 17:43:00 -0300

    Implementação do método excluir e editar da classe GenericDAO e implementação dos testes para estes métidos na classe EstadoDAOTest

--- src/test/java/br/com/drogaria/dao/EstadoDAOTest.java
+++ src/test/java/br/com/drogaria/dao/EstadoDAOTest.java
@@ -34,6 +34,7 @@ public class EstadoDAOTest {
 	}
 
 	@Test
+	@Ignore
 	public void buscar() {
 		Long codigo = 3L;
 		EstadoDAO estadoDAO = new EstadoDAO();
@@ -45,6 +46,46 @@ public class EstadoDAOTest {
 			System.out.println("Registro encontrado: ");
 			System.out.println(estado.getNome() + " - " + estado.getSigla());
 		}
+	}
+
+	@Test
+	@Ignore
+	public void excluir() {
+		Long codigo = 5L;
+
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigo);
+
+		if (estado == null) {
+			System.out.println("Nenhum estado encontrado.");
+		} else {
+			estadoDAO.excluir(estado);
+			System.out.println("Estado " + estado.getNome()
+					+ " removido com sucesso.");
+		}
+	}
+
+	@Test
+	public void editar() {
+		Long codigo = 4L;
+
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigo);
+
+		if (estado == null) {
+			System.out.println("Nenhum registro encontrado;");
+		} else {
+			System.out.println("Estado " + estado.getNome() + "/"
+					+ estado.getSigla() + " alterado para: ");
+			estado.setNome("Minas Gerais");
+			estado.setSigla("MG");
+			estadoDAO.editar(estado);
+
+			estado = estadoDAO.buscar(codigo);
+
+			System.out.println(estado.getNome() + "/" + estado.getSigla());
+		}
+
 	}
 
 }

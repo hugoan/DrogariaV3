37c17016f955940bc17f63c9ba054c31018b8140
Hugo <hugo.nobreg@gmail.com>
2016-08-22 22:46:44 -0300

    Inicialização da Bean e ajuste da view para teste.

---
+++ src/main/java/br/com/drogariav3/bean/EstadoBean.java
@@ -0,0 +1,11 @@
+package br.com.drogariav3.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class EstadoBean {
+	public void salvar(){
+		System.out.println("Programação Java Web,");
+	}
+
+}

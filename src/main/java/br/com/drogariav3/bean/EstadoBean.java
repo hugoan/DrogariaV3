6cb8b00b5fd17247960ac5f7b7d53342533146a2
Hugo <hugo.nobreg@gmail.com>
2016-08-29 22:35:27 -0300

    Implementação da classe bean

--- src/main/java/br/com/drogariav3/bean/EstadoBean.java
+++ src/main/java/br/com/drogariav3/bean/EstadoBean.java
@@ -1,11 +1,45 @@
 package br.com.drogariav3.bean;
 
-import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.com.drogariav3.dao.EstadoDAO;
+import br.com.drogariav3.domain.Estado;
+
+@SuppressWarnings("serial")
 @ManagedBean
-public class EstadoBean {
-	public void salvar(){
-		System.out.println("Programação Java Web,");
+@ViewScoped
+public class EstadoBean implements Serializable {
+	private Estado estado;
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public void novo() {
+		estado = new Estado();
+	}
+
+	public void salvar() {
+		try {
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estadoDAO.salvar(estado);
+
+			novo();
+
+			Messages.addFlashGlobalInfo("Estado salvo com sucesso!");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar um estado");
+			erro.printStackTrace();
+		}
 	}
 
 }

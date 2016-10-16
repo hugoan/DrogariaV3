7b5dc50da766d663dda39f556d01b1230b8f242f
Hugo <hugo.nobreg@gmail.com>
2016-10-15 22:56:32 -0300

    Implementação do DAO e tela.

--- src/main/java/br/com/drogariav3/bean/EstadoBean.java
+++ src/main/java/br/com/drogariav3/bean/EstadoBean.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
@@ -35,8 +36,8 @@ public class EstadoBean implements Serializable {
 		this.estados = estados;
 	}
 
-	//Anotação para chamar o método após que a bean é criada.
-	//Dessa forma o formulário é preenchido no momento que a view é aberta.
+	// Anotação para chamar o método após que a bean é criada.
+	// Dessa forma o formulário é preenchido no momento que a view é aberta.
 	@PostConstruct
 	public void listar() {
 		try {
@@ -55,15 +56,37 @@ public class EstadoBean implements Serializable {
 	public void salvar() {
 		try {
 			EstadoDAO estadoDAO = new EstadoDAO();
-			estadoDAO.salvar(estado);
+			estadoDAO.merge(estado);
 
 			novo();
 
 			Messages.addFlashGlobalInfo("Estado salvo com sucesso!");
+
+			estados = estadoDAO.listar();
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar salvar um estado");
 			erro.printStackTrace();
 		}
+	}
+
+	public void excluir(ActionEvent evento) {
+		try {
+			estado = (Estado) evento.getComponent().getAttributes().get("estadoSelecionado");
+
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estadoDAO.excluir(estado);
+			Messages.addGlobalInfo("Estado removido com sucesso.");
+			estados = estadoDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar remover o estado");
+			erro.printStackTrace();
+
+		}
+	}
+	
+	public void editar(ActionEvent evento){
+		estado = (Estado) evento.getComponent().getAttributes().get("estadoSelecionado");
+		
 	}
 
 }

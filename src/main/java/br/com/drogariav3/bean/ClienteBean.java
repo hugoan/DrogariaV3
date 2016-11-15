45962bbde69b2da0854c84d69f8f29da940a82db
Hugo <hugo.nobreg@gmail.com>
2016-11-15 17:51:25 -0200

    Tradução do calendário

--- src/main/java/br/com/drogariav3/bean/ClienteBean.java
+++ src/main/java/br/com/drogariav3/bean/ClienteBean.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
@@ -19,80 +20,104 @@ import br.com.drogariav3.domain.Pessoa;
 @ViewScoped
 public class ClienteBean implements Serializable {
 	private Cliente cliente;
-	
+
 	private List<Cliente> clientes;
 	private List<Pessoa> pessoas;
-	
+
 	public Cliente getCliente() {
 		return cliente;
 	}
-	
+
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
-	
+
 	public List<Cliente> getClientes() {
 		return clientes;
 	}
-	
+
 	public void setClientes(List<Cliente> clientes) {
 		this.clientes = clientes;
 	}
-	
+
 	public List<Pessoa> getPessoas() {
 		return pessoas;
 	}
-	
+
 	public void setPessoas(List<Pessoa> pessoas) {
 		this.pessoas = pessoas;
 	}
-	
+
 	@PostConstruct
-	public void listar(){
-		try{
+	public void listar() {
+		try {
 			ClienteDAO clienteDAO = new ClienteDAO();
 			clientes = clienteDAO.listar("dataCadastro");
-		} catch(RuntimeException erro){
+		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar listar os clientes");
 			erro.printStackTrace();
 		}
 	}
-	
-	public void novo(){
-		try{
-		cliente = new Cliente();
-		
-		PessoaDAO pessoaDAO = new PessoaDAO();
-		pessoas = pessoaDAO.listar("nome");
-		} catch(RuntimeException erro){
-			if(pessoas != null){
-			Messages.addGlobalError("Ocorreu um erro ao tentar criar um novo cliente");
-			erro.printStackTrace();
+
+	public void novo() {
+		try {
+			cliente = new Cliente();
+
+			PessoaDAO pessoaDAO = new PessoaDAO();
+			pessoas = pessoaDAO.listar("nome");
+		} catch (RuntimeException erro) {
+			if (pessoas != null) {
+				Messages.addGlobalError("Ocorreu um erro ao tentar criar um novo cliente");
+				erro.printStackTrace();
 			} else {
 				Messages.addGlobalError("Nenhum registro de pessoa encontrado.");
 				erro.printStackTrace();
 			}
 		}
 	}
-	
-	public void salvar(){
-		try{
+
+	public void salvar() {
+		try {
 			ClienteDAO clienteDAO = new ClienteDAO();
 			clienteDAO.merge(cliente);
-			
+
 			cliente = new Cliente();
-			
+
 			clientes = clienteDAO.listar("dataCadastro");
-			
+
 			PessoaDAO pessoaDAO = new PessoaDAO();
 			pessoas = pessoaDAO.listar("nome");
-			
+
 			Messages.addGlobalInfo("Cliente salvo com sucesso");
-			
-			} catch(RuntimeException erro){
+
+		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o cliente");
 			erro.printStackTrace();
 		}
+	}
+
+	public void excluir(ActionEvent evento) {
+		try {
+			cliente = (Cliente) evento.getComponent().getAttributes().get("clienteSelecionado");
+
+			ClienteDAO clienteDAO = new ClienteDAO();
+			clienteDAO.excluir(cliente);
+
+			clientes = clienteDAO.listar("dataCadastro");
+
+			Messages.addGlobalInfo("Cliente removido com sucesso");
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar excluir um cliente");
+			erro.printStackTrace();
+		}
+	}
+
+	public void editar(ActionEvent evento) {
+		cliente = (Cliente) evento.getComponent().getAttributes().get("clienteSelecionado");
+
+		PessoaDAO pessoaDAO = new PessoaDAO();
+		pessoas = pessoaDAO.listar("nome");
 	}
 
 }

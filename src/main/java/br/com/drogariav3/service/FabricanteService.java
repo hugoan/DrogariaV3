4e342e79da821289c785a6870f2b8914ab36f6f1
Hugo <hugo.nobreg@gmail.com>
2016-11-23 21:58:32 -0200

    Chamadas de serviços Json - listar, buscar, salvar e editar

--- src/main/java/br/com/drogariav3/service/FabricanteService.java
+++ src/main/java/br/com/drogariav3/service/FabricanteService.java
@@ -2,7 +2,10 @@ package br.com.drogariav3.service;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -13,33 +16,76 @@ import br.com.drogariav3.domain.Fabricante;
 
 @Path("fabricante")
 public class FabricanteService {
-	
-	//http://http://localhost:8080/DrogariaV3/rest/fabricante
+
+	// http://http://localhost:8080/DrogariaV3/rest/fabricante
 	@GET
-	public String listar(){
+	public String listar() {
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		List<Fabricante> fabricantes = fabricanteDAO.listar("descricao");
-		
+
 		Gson gson = new Gson();
 		String json = gson.toJson(fabricantes);
-		
+
 		return json;
-		
-		
+
 	}
-	
-	//http://localhost:8080/DrogariaV3/rest/fabricante/codigo => no lugar do código informo o id que quero buscar
-	//http://localhost:8080/DrogariaV3/rest/fabricante/2 => Exemplo
+
+	// http://localhost:8080/DrogariaV3/rest/fabricante/codigo => no lugar do
+	// código informo o id que quero buscar
+	// http://localhost:8080/DrogariaV3/rest/fabricante/2 => Exemplo
 	@GET
-	@Path("{codigo}") //Cada vez que coloco o path é uma barra no endereço.
-	public String buscar(@PathParam("codigo") Long codigo){
+	@Path("{codigo}") // Cada vez que coloco o path é uma barra no endereço.
+	public String buscar(@PathParam("codigo") Long codigo) {
 		FabricanteDAO fabricanteDAO = new FabricanteDAO();
 		Fabricante fabricante = fabricanteDAO.buscar(codigo);
-		
+
 		Gson gson = new Gson();
 		String json = gson.toJson(fabricante);
-		
+
 		return json;
+	}
+
+	// http://http://localhost:8080/DrogariaV3/rest/fabricante
+	@POST
+	public String salvar(String json) {
+		Gson gson = new Gson();
+		Fabricante fabricante = gson.fromJson(json, Fabricante.class);
+
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricanteDAO.salvar(fabricante);
+
+		String jsonSaida = gson.toJson(fabricante);
+
+		return jsonSaida;
+	}
+
+	// http://http://localhost:8080/DrogariaV3/rest/fabricante
+	@PUT
+	public String editar(String json) {
+		Gson gson = new Gson();
+		Fabricante fabricante = gson.fromJson(json, Fabricante.class);
+
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricanteDAO.editar(fabricante);
+
+		String jsonSaida = gson.toJson(fabricante);
+
+		return jsonSaida;
+	}
+
+	// http://localhost:8080/DrogariaV3/rest/fabricante/codigo
+	@DELETE
+	public String excluir(String json) {
+		Gson gson = new Gson();
+		Fabricante fabricante = gson.fromJson(json, Fabricante.class);
+
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricante = fabricanteDAO.buscar(fabricante.getCodigo());
+		fabricanteDAO.excluir(fabricante);
+
+		String saida = gson.toJson(fabricante);
+
+		return saida;
 	}
 
 }
